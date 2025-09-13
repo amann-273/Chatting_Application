34ab951738b86fd857b5843ba08fd2693f611669
Mohd Aman <118279443+amann-273@users.noreply.github.com>
2025-09-13 23:07:19 +0530

    Updated commit

--- src/chatting/applicarion/Server.java
+++ src/chatting/applicarion/Server.java
@@ -136,7 +136,8 @@ public class Server implements ActionListener
         }
     }
     
-    public static JPanel formatLabel(String out) {
+    public static JPanel formatLabel(String out) 
+    {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         
